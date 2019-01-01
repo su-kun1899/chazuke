54660089bee29d7bc8228886317417af3055e293
su-kun1899 <higedrum.coz@gmail.com>
2019-01-01 16:30:05 +0900

    mapで取得するメソッドを用意する

--- chazuke_test.go
+++ chazuke_test.go
@@ -1,6 +1,7 @@
 package chazuke_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/su-kun1899/chazuke"
@@ -52,6 +53,57 @@ func TestJSONContainer_Value(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("JSONContainer.Value() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestJSONContainer_Values(t *testing.T) {
+	jsonVal := `
+		{
+			"team": "FC Barcelona",
+			"manager": {
+				"name": "Ernest Valverde",
+				"birthDay": "1964-02-09"
+			},
+			"players":[
+        		{"name":"Messi", "position":"Forward"}, 
+        		{"name":"Coutinho", "position":"Midfielder"},
+        		{"name":"Pique", "position":"Defender"}
+		  	]
+		}
+	`
+
+	tests := []struct {
+		name    string
+		key     string
+		want    map[string]interface{}
+		wantErr bool
+	}{
+		{
+			name: "Get manager values",
+			key:  "manager",
+			want: map[string]interface{}{
+				"name":     "Ernest Valverde",
+				"birthDay": "1964-02-09",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			container, err := chazuke.New(jsonVal)
+			if err != nil {
+				t.Fatal("unexpected error:", err)
+			}
+
+			got, err := container.Get(tt.key).Values()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("JSONContainer.Values() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("JSONContainer.Values() = %v, want %v", got, tt.want)
 			}
 		})
 	}
