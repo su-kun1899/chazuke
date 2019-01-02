12ce20817db3f16a13c9d4f110bffd4d9703fbc3
su-kun1899 <higedrum.coz@gmail.com>
2019-01-02 14:46:34 +0900

    Add test: get nested value

--- chazuke_test.go
+++ chazuke_test.go
@@ -108,3 +108,53 @@ func TestJSONContainer_Values(t *testing.T) {
 		})
 	}
 }
+
+func TestJSONContainer_NestedValue(t *testing.T) {
+	jsonVal := `
+		{
+			"team": "FC Barcelona",
+			"manager": {
+				"name": "Ernest Valverde",
+				"birthDay": "1964-02-09"
+			},
+			"players":[
+        		{"name":"Messi", "position":"Forward"}, 
+        		{"name":"Coutinho", "position":"Midfielder"},
+        		{"name":"Pique", "position":"Defender"}
+		  	]
+		}
+	`
+
+	tests := []struct {
+		name      string
+		parentKey string
+		childKey  string
+		want      string
+		wantErr   bool
+	}{
+		{
+			name:      "Get manager values",
+			parentKey: "manager",
+			childKey:  "name",
+			want:      "Ernest Valverde",
+			wantErr:   false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			container, err := chazuke.New(jsonVal)
+			if err != nil {
+				t.Fatal("unexpected error:", err)
+			}
+
+			got, err := container.Get(tt.parentKey).Get(tt.childKey).Value()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("JSONContainer.Value() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("JSONContainer.Value() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
