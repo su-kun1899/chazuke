159092273dd176c6f4cad2e94bee463d6280592e
su-kun1899 <higedrum.coz@gmail.com>
2019-01-13 17:46:51 +0900

    add test case

--- chazuke_test.go
+++ chazuke_test.go
@@ -332,6 +332,12 @@ func TestNew(t *testing.T) {
 			want:    nil,
 			wantErr: true,
 		},
+		{
+			name:    "empty args",
+			args:    args{jsonVal: ""},
+			want:    nil,
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
