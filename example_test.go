c24a8172369773679789d0695d77f41a07c60c0f
su-kun1899 <higedrum.coz@gmail.com>
2019-01-20 11:57:18 +0900

    add example of Array()

--- example_test.go
+++ example_test.go
@@ -37,3 +37,38 @@ func ExampleContainer_Value() {
 	// FC Barcelona
 	// 1964-02-09
 }
+
+func ExampleContainer_Array() {
+	jsonVal := `
+    {
+        "players":[
+            {"name":"Messi", "position":"Forward"},
+            {"name":"Coutinho", "position":"Midfielder"},
+            {"name":"Pique", "position":"Defender"}
+        ]
+    }
+    `
+
+	container, err := chazuke.New(jsonVal)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	players, err := container.Get("players").Array()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, p := range players {
+		name, err := p.Get("name").Value()
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(name)
+	}
+
+	// Output:
+	// Messi
+	// Coutinho
+	// Pique
+}
